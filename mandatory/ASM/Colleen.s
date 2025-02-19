f793da7d7c1e72defd32a67c547564ded4adc8d9
Axel Charlot <acharlot@c2r4p4.42lehavre.fr>
2025-02-19 17:07:04 +0100

    Started to work on the asm files

--- mandatory/ASM/Colleen.s
+++ mandatory/ASM/Colleen.s
@@ -1,27 +1,24 @@
+global main
+extern printf
+;One comment
+colleen:
+mov rdi, format
+mov rsi, 10
+mov rcx, 34
+mov r8, format
+push rbx
+call printf wrt ..plt
+pop rbx
+ret
+
+main:
+push rbx
+call colleen
+pop rbx
+;Another comment
+mov rax, 0
+ret
+
 section .data
-    fmt db 'section .data',10,'    fmt db %c%s%c,10,%c%s%c,10,0',10
-    newline db 10,0
-
-section .text
-    global _start
-
-_start:
-    mov rdi, fmt
-    mov rsi, 39     ; ASCII for '
-    mov rdx, fmt
-    mov rcx, 34     ; ASCII for "
-    mov r8, newline
-    call print
-    call exit
-
-print:
-    mov rax, 1       ; sys_write
-    mov rdi, 1       ; stdout
-    mov rdx, 100     ; length
-    syscall
-    ret
-
-exit:
-    mov rax, 60      ; sys_exit
-    xor rdi, rdi
-    syscall
+format:
+db "global main%1$cextern printf%1$c;One comment%1$ccolleen:%1$c%2$cmov rdi, format%1$cmov rsi, 10%1$cmov rcx, 34%1$cmov r8, format%1$cpush rbx%1$ccall printf wrt ..plt%1$cpop rbx%1$cret%1$c%1$cmain:%1$cpush rbx%1$ccall colleen%1$cpop rbx%1$c;Another comment%1$c; Return 0%1$c;%1$cmov rax, 0%1$c%ret%1$c%1$csection .data%1$cformat:%1$cdb %2$c%3$s%2$c%1$c%1$c"
