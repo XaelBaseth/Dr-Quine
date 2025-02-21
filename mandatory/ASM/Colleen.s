70bac174917428a81a7b549a5c4652ca1d31d3d5
Axel Charlot <acharlot@c2r4p4.42lehavre.fr>
2025-02-21 17:07:14 +0100

    colleen done

--- mandatory/ASM/Colleen.s
+++ mandatory/ASM/Colleen.s
@@ -1,24 +1,27 @@
-global main
-extern printf
-;One comment
-colleen:
-mov rdi, format
-mov rsi, 10
-mov rcx, 34
-mov r8, format
-push rbx
-call printf wrt ..plt
-pop rbx
-ret
-
-main:
-push rbx
-call colleen
-pop rbx
-;Another comment
-mov rax, 0
-ret
+; One comment
 
 section .data
-format:
-db "global main%1$cextern printf%1$c;One comment%1$ccolleen:%1$c%2$cmov rdi, format%1$cmov rsi, 10%1$cmov rcx, 34%1$cmov r8, format%1$cpush rbx%1$ccall printf wrt ..plt%1$cpop rbx%1$cret%1$c%1$cmain:%1$cpush rbx%1$ccall colleen%1$cpop rbx%1$c;Another comment%1$c; Return 0%1$c;%1$cmov rax, 0%1$c%ret%1$c%1$csection .data%1$cformat:%1$cdb %2$c%3$s%2$c%1$c%1$c"
+file_content:
+	db "; One comment%2$c%2$csection .data%2$cfile_content:%2$c%4$cdb %3$c%1$s%3$c, 0%2$c%2$csection .text%2$c%4$cglobal main%2$c%4$cextern printf%2$c%2$cstart:%2$c%4$ccall main%2$c%4$cret%2$c%2$cmain:%2$c%4$c; Other comment%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$clea rdi, [rel file_content]%2$c%4$clea rsi, [rel file_content]%2$c%4$cmov rdx, 10%2$c%4$cmov rcx, 34%2$c%4$cmov r8, 9%2$c%4$ccall printf%2$c%4$cmov rsp, rbp%2$c%4$cpop rbp%2$c%4$cret%2$c", 0
+
+section .text
+	global main
+	extern printf
+
+start:
+	call main
+	ret
+
+main:
+	; Other comment
+	push rbp
+	mov rbp, rsp
+	lea rdi, [rel file_content]
+	lea rsi, [rel file_content]
+	mov rdx, 10
+	mov rcx, 34
+	mov r8, 9
+	call printf
+	mov rsp, rbp
+	pop rbp
+	ret
