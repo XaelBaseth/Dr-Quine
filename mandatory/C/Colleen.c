0788a9cdc822d80191f914d69f99fd9d3c30712f
Axel Charlot <acharlot@c2r4p4.42lehavre.fr>
2025-03-10 09:40:55 +0100

    corrected the comments accordingly to the subject requirements

--- mandatory/C/Colleen.c
+++ mandatory/C/Colleen.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
-/* This is an external comment */
-void function(char*s){/* This is a comment inside the function */printf(s, 10, 10, 10, 34, s, 34, 10, 10);}
-int main(void){char*s="#include <stdio.h>%c/* This is an external comment */%cvoid function(char*s){/* This is a comment inside the function */printf(s, 10, 10, 10, 34, s, 34, 10, 10);}%cint main(void){char*s=%c%s%c;function(s);return 0;}%c";function(s);return 0;}
+/*
+This is an external comment
+*/
+void function(char*s){/*
+This is a comment inside the function
+*/printf(s, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10);}
+int main(void){char*s="#include <stdio.h>%c/*%cThis is an external comment%c*/%cvoid function(char*s){/*%cThis is a comment inside the function%c*/printf(s, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10);}%cint main(void){char*s=%c%s%c;function(s);return 0;}%c";function(s);return 0;}
