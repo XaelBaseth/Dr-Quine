6557b7277448513777b6b6cc411f0317551beaad
Axel Charlot <acharlot@c2r4p4.42lehavre.fr>
2025-03-11 16:12:53 +0100

    Colleen & Grace bonus

--- bonus/golang/Grace.go
+++ bonus/golang/Grace.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const filename = "./Grace_kid.go"
+const format = "package main%[1]c%[1]cimport (%[3]cfmt%[3]c; %[3]cos%[3]c)%[1]c%[1]cconst filename = %[3]cGrace_kid.go%[3]c%[1]cconst format = %[3]c%[4]s%[3]c%[1]c%[1]cfunc main() {%[1]c%[2]cfile, _ := os.Create(filename)%[1]c%[2]cdefer file.Close()%[1]c%[2]cfmt.Fprintf(file, format, 10, 9, 34, format)%[1]c}%[1]c"
+
+func main() {
+	file, _ := os.Create(filename)
+	defer file.Close()
+	fmt.Fprintf(file, format, 10, 9, 34, format)
+}
