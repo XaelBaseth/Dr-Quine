db960142a694f19ef1a5824efee059d68ebea243
Axel Charlot <acharlot@c2r4p4.42lehavre.fr>
2025-03-12 15:40:01 +0100

    All bonus completed

--- bonus/golang/Sully.go
+++ bonus/golang/Sully.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+const code = "package main%[1]cimport (%[1]c%[2]c%[3]cfmt%[3]c%[1]c%[2]c%[3]cos%[3]c%[1]c%[2]c%[3]cos/exec%[3]c%[1]c)%[1]c%[1]cconst code = %[3]c%[4]s%[3]c%[1]c%[1]cfunc main() {%[1]c%[2]ci := %[5]d%[1]c%[2]cif i <= 0 {%[1]c%[2]c%[2]creturn%[1]c%[2]c}%[1]c%[1]c%[2]cnewSrc := fmt.Sprintf(%[3]cSully_%%d.go%[3]c, i)%[1]c%[2]cnewExec := fmt.Sprintf(%[3]cSully_%%d%[3]c, i)%[1]c%[2]cfile, _ := os.Create(newSrc)%[1]c%[2]cdefer file.Close()%[1]c%[1]c%[2]cfmt.Fprintf(file, code, 10, 9, 34, code, i-1)%[1]c%[1]c%[2]cexec.Command(%[3]cgo%[3]c, %[3]cbuild%[3]c, %[3]c-o%[3]c, newExec, newSrc).Run()%[1]c%[2]ccmd := exec.Command(%[3]c./%[3]c + newExec)%[1]c%[2]ccmd.Start()%[1]c}%[1]c"
+
+func main() {
+	i := 5
+	if i <= 0 {
+		return
+	}
+
+	newSrc := fmt.Sprintf("Sully_%d.go", i)
+	newExec := fmt.Sprintf("Sully_%d", i)
+
+	file, _ := os.Create(newSrc)
+	defer file.Close()
+
+	fmt.Fprintf(file, code, 10, 9, 34, code, i-1)
+
+	exec.Command("go", "build", "-o", newExec, newSrc).Run()
+	cmd := exec.Command("./" + newExec)
+	cmd.Start()
+}
--- mandatory/C/Grace.c
+++ mandatory/C/Grace.c
@@ -4,6 +4,6 @@ One comment
 */
 #define FN "Grace_kid.c"
 #define printout(...)fprintf(fopen(FN,"w"),__VA_ARGS__);
-#define FT(x)int main(){printout(x,10, 10, 10, 10, 34, 34, 10, 34, 34, 10, 10, 34, x, 34, 10, 10)}
-char*s="#include <stdio.h>%c/*%cOne comment%c*/%c#define FN %cGrace_kid.c%c%c#define printout(...)fprintf(fopen(FN,%cw%c),__VA_ARGS__);%c#define FT(x)int main(){printout(x,10, 10, 10, 10, 34, 34, 10, 34, 34, 10, 10, 34, x, 34, 10, 10)}%cchar*s=%c%s%c;%cFT(s)%c";
+#define FT(x)int main(){printout(x, 10, 34, x)}
+char *s = "#include <stdio.h>%1$c/*%1$cOne comment%1$c*/%1$c#define FN %2$cGrace_kid.c%2$c%1$c#define printout(...)fprintf(fopen(FN,%2$cw%2$c),__VA_ARGS__);%1$c#define FT(x)int main(){printout(x, 10, 34, x)}%1$cchar *s = %2$c%3$s%2$c;%1$cFT(s)%1$c";
 FT(s)
